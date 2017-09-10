748f51d295b4e6cfcf83b2570588aacb550012e9
dragon0429 <dragon0429@yahoo.com>
2017-09-10 09:11:18 +0300

    session finished

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -14,12 +14,16 @@ def index(request):
     # Available books (status = 'a')
     num_instances_available=BookInstance.objects.filter(status__exact='a').count()
     num_authors=Author.objects.count()  # The 'all()' is implied by default.
+
+    # Number of visits to this view, as counted in the session variable.
+    num_visits=request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits+1
     
     # Render the HTML template index.html with the data in the context variable
     return render(
         request,
         'index.html',
-        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
+        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors,'num_visits':num_visits},
     )
 
 from django.views import generic
@@ -30,3 +34,10 @@ class BookListView(generic.ListView):
 
 class BookDetailView(generic.DetailView):
     model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 1
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
